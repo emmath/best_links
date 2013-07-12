6345458dab56541c8b8e1f4fbbaf25a89bf26bda
Emma Hardman <emmatrevena@gmail.com>
2013-07-11 20:43:40 -0700

    Generate Best Links application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BestLinks::Application.config.secret_token = '2c987a3e8a28e8613abe6c1d9b8f0e92aca43caec353fefc267cea578e9de9f037db0d0a9a9b21e88bfc14c9a87807eccec78f4c459d1b59e8c54b3ca910371b'
